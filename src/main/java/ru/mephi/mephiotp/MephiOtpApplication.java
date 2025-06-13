b350bfd5b6a9a379b77adc7fc44096e076afab6d
Roman <astek14@mail.ru>
2025-06-13 21:44:09 +0300

    Initial commit

---
+++ src/main/java/ru/mephi/mephiotp/MephiOtpApplication.java
@@ -0,0 +1,13 @@
+package ru.mephi.mephiotp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MephiOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MephiOtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mephi/mephiotp/MephiOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mephi.mephiotp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MephiOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
